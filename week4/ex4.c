8bd39f61929196994adc20ba16399767c3197db3
RainCatalyst <freezedice@yandex.ru>
2021-09-10 00:38:35 +0300

    ex 3,4 for week 4

--- week4/ex4.c
+++ week4/ex4.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -7,23 +8,37 @@
 #define MAX_COMMAND_SIZE 100
 #define MAX_ARGS_COUNT 10
 
-int main(void)
+int tokenize(char str[], char* tokens[], char* s)
 {
-    char command[MAX_COMMAND_SIZE];
-    char* args[MAX_ARGS_COUNT];
-    const char s = ' ';
-
-    fgets(command, MAX_COMMAND_SIZE, stdin);
-    
-    int tokens = 0;
-    char* token = strtok(command, s);
-    char* cmd = strtok(NULL, s);
+    int token_count = 0;
+    char* token = strtok(str, s);
     while (token != NULL)
     {
-        args[tokens] = token;
-        tokens += 1;
+        tokens[token_count] = token;
+        
+        token = strtok(NULL, s);
+        token_count++;
     }
+    return token_count;
+}
 
-    execvp(cmd, args);
+int main(void)
+{
+    char input[MAX_COMMAND_SIZE];
+    char* args[MAX_ARGS_COUNT] = {NULL};
+
+    fgets(input, MAX_COMMAND_SIZE, stdin);
+
+    int token_count = tokenize(input, args, " \n");
+    
+    pid_t PID = fork();
+    if (PID == 0)
+    {
+        execvp(args[0], args);
+    }else{
+        printf("Running command in a child process (PID=%d)\n\n", PID);
+        wait(NULL);
+        printf("\nDone.\n");
+    }
     return EXIT_SUCCESS;
 }
